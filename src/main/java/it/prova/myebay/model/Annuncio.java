7ca3a764f68afe58d9b4ab1d23bd83123abebe03
PerottiIrene <ireneperotti29@gmail.com>
2022-11-18 22:05:09 +0100

    implementate classi di service

--- src/main/java/it/prova/myebay/model/Annuncio.java
+++ src/main/java/it/prova/myebay/model/Annuncio.java
@@ -31,8 +31,6 @@ public class Annuncio {
 	private Integer prezzo;
 	@Column(name = "aperto")
 	private boolean aperto;
-	@Column(name = "cognome")
-	private String cognome;
 	@Column(name = "data")
 	private Date data;
 	
@@ -43,6 +41,76 @@ public class Annuncio {
 	@ManyToMany
 	@JoinTable(name = "annuncio_categoria", joinColumns = @JoinColumn(name = "annuncio_id", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "categoria_id", referencedColumnName = "ID"))
 	private Set<Categoria> categorie = new HashSet<>();
+
+	public Annuncio(Long id, String testoAnnuncio, Integer prezzo, boolean aperto, Date data, Utente utenteInserimento,
+			Set<Categoria> categorie) {
+		super();
+		this.id = id;
+		this.testoAnnuncio = testoAnnuncio;
+		this.prezzo = prezzo;
+		this.aperto = aperto;
+		this.data = data;
+		this.utenteInserimento = utenteInserimento;
+		this.categorie = categorie;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTestoAnnuncio() {
+		return testoAnnuncio;
+	}
+
+	public void setTestoAnnuncio(String testoAnnuncio) {
+		this.testoAnnuncio = testoAnnuncio;
+	}
+
+	public Integer getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(Integer prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public boolean isAperto() {
+		return aperto;
+	}
+
+	public void setAperto(boolean aperto) {
+		this.aperto = aperto;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Utente getUtenteInserimento() {
+		return utenteInserimento;
+	}
+
+	public void setUtenteInserimento(Utente utenteInserimento) {
+		this.utenteInserimento = utenteInserimento;
+	}
+
+	public Set<Categoria> getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Set<Categoria> categorie) {
+		this.categorie = categorie;
+	}
+	
+	
 	
 	
 
