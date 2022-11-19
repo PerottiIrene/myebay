cff960ad3fbf5e4dbaf1a46ca59fcdd58e07282e
PerottiIrene <ireneperotti29@gmail.com>
2022-11-19 20:56:39 +0100

    implementata insert e search utente

--- src/main/java/it/prova/myebay/service/UtenteServiceImpl.java
+++ src/main/java/it/prova/myebay/service/UtenteServiceImpl.java
@@ -1,8 +1,16 @@
 package it.prova.myebay.service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +28,9 @@ public class UtenteServiceImpl implements UtenteService{
 	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Transactional(readOnly = true)
 	public List<Utente> listAllUtenti() {
@@ -64,9 +75,43 @@ public class UtenteServiceImpl implements UtenteService{
 
 	@Transactional(readOnly = true)
 	public List<Utente> findByExample(Utente example) {
-		// TODO Da implementare
-		return listAllUtenti();
+		Map<String, Object> paramaterMap = new HashMap<String, Object>();
+		List<String> whereClauses = new ArrayList<String>();
+
+		StringBuilder queryBuilder = new StringBuilder("select u from Utente u where u.id = u.id ");
+
+		if (StringUtils.isNotEmpty(example.getUsername())) {
+			whereClauses.add(" u.username  like :username ");
+			paramaterMap.put("username", "%" + example.getUsername() + "%");
+		}
+		if (StringUtils.isNotEmpty(example.getNome())) {
+			whereClauses.add(" u.nome like :nome ");
+			paramaterMap.put("nome", "%" + example.getNome() + "%");
+		}
+		if (StringUtils.isNotEmpty(example.getCognome())) {
+			whereClauses.add(" u.cognome like :cognome ");
+			paramaterMap.put("cognome", "%" + example.getCognome() + "%");
+		}
+		if (example.getDateCreated() != null) {
+			whereClauses.add("u.dateCreated >= :dateCreated ");
+			paramaterMap.put("dateCreated", example.getDateCreated());
+		}
+		
+		if (example.getCreditoResiduo() != null) {
+			whereClauses.add("u.creditoResiduo >= :creditoResiduo ");
+			paramaterMap.put("creditoResiduo", example.getCreditoResiduo());
+		}
+		
+		queryBuilder.append(!whereClauses.isEmpty()?" and ":"");
+		queryBuilder.append(StringUtils.join(whereClauses, " and "));
+		TypedQuery<Utente> typedQuery = entityManager.createQuery(queryBuilder.toString(), Utente.class);
+
+		for (String key : paramaterMap.keySet()) {
+			typedQuery.setParameter(key, paramaterMap.get(key));
+		}
+		return typedQuery.getResultList();
 	}
+	
 
 	@Transactional(readOnly = true)
 	public Utente eseguiAccesso(String username, String password) {
